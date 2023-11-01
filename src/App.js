9b7ae0cfd6a31101bafee058463aa87f164ec369
JAVED1812116 <73031861+JAVED1812116@users.noreply.github.com>
2023-11-01 12:12:24 +0500

    palindrome work done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [txt,setText]=useState(null)
+  const [show,setShow]=useState(null)
+  const [result,setResult]=useState(true)
+
+  function paliDrom(txt){
+    setShow(txt)
+    let start=0;
+      let end=txt.length-1;
+      while(end>start){
+        if(txt[start]!==txt[end]){
+          setResult(false)
+        }
+        else if(txt[start]===txt[end]){
+          setResult(true)
+        }
+        start++;
+        end--;
+      }
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="fullDiv">
+
+      <h1 id="h1">Palindrome</h1>
+    <div className="inpt">
+      <input placeholder="Type Text Here" onChange={((e)=>{setText(e.target.value)})}/>
     </div>
+    
+    { txt?.length>0?
+    <div style={{marginTop:35}}>
+    <button onClick={()=>{paliDrom(txt)}}>Click</button>
+    </div>:''
+    }
+    <div>
+     
+        {
+          show?.length>0 &&txt?.length>0?
+        result===true?<div style={{color:'green',fontSize:'24px'}}>{show+' '+'is a Palidrome'}</div>:<div style={{color:'red'}}>{show+' '+"is not a palidrome"}</div>:<div style={{color:"orange"}}>Enter Some Values </div>
+        }
+    </div>
+    </div>
+    </div>
+
   );
 }
 
